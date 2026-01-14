b04ad115ec4e5ee186421465b9886870f31608e8
mjstierman <matthew.stierman@alumni.du.edu>
2026-01-14 13:10:57 -0700

    Prettified prereq checks. Refactored tests into its own file.

--- hpc-in-a-box.sh
+++ hpc-in-a-box.sh
@@ -1,24 +1,60 @@
 #!/bin/bash
 
 ### Check prequisites ###
+# Which Linux distro are we on?
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO=$ID
+else
+    echo "Can't determine Linux distro. Exiting."
+    exit 1
+fi
+
+# Determine package manager
+if [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ]; then
+    PKG_MGR="apt"
+elif [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ] || [ "$DISTRO" == "fedora" ]; then
+    PKG_MGR="yum"
+else
+    echo "Unsupported Linux distro: $DISTRO. Exiting."
+    exit 1
+fi
+
 # Make sure the software is installed
 echo "Running prechecks..."
-vagrant --version || (echo "Whoops! Can't find Vagrant. Is it installed?"; exit)
-ansible --version || (echo "Whoops! Can't find Ansible. Is it installed?"; exit)
-vboxmanage --version || (echo "Whoops! Can't find VirtualBox. Is it installed?"; exit)
+echo "Checking for vagrant..."
+vagrant --version 2&> /dev/null && echo -e "\e[32m pass\e[0m" || { echo -e "\e[31m faileds\e[0m Try: $PKG_MGR install vagrant"; exit 1; }
+echo "Checking for ansible..."
+ansible --version 2&> /dev/null && echo -e "\e[32m pass\e[0m"|| { echo -e "\e[31m faileds\e[0m Try: $PKG_MGR install ansible"; exit 1; }
+echo "Checking for VirtualBox..."
+vboxmanage --version 2&> /dev/null && echo -e "\e[32m pass\e[0m"|| { echo -e "\e[31m faileds\e[0m Try: $PKG_MGR install ansible?"; exit 1; }
+echo "Checking for sshpass..."
+sshpass -V 2&> /dev/null && echo -e "\e[32m pass\e[0m"|| { echo -e "\e[31m failed\e[0m \n Try: $PKG_MGR install sshpass"; exit 1; }
 echo "Completed prechecks."
 
 ### Generate VMs in virtulbox ###
 # Run vagrantfile
-vagrant up || echo "Whoops! Something went wrong. Is the Vagrantfile in your current working directory?"
+echo "Bringing up Vagrant VMs. This may take a few minutes..."
+vagrant up || { echo "Something went wrong. Is the Vagrantfile in your current working directory?"; exit 1; }
 
 ### Process output from VirtualBox ###
-# Make executable the inventory creation script
-chmod +x ./create_inventories.sh
+echo "Checking for inventory creation script..."
+if [ ! -f ./create_inventories.sh ]; then
+    echo "Inventory creation script not found! Exiting."
+    exit 1
+fi
+if [ -x ./create_inventories.sh ]; then
+    echo "Inventory creation script is executable."
+else
+    echo "Making inventory creation script executable."
+    chmod +x ./create_inventories.sh
+fi
 # Call another script to create the Ansible inventory and control_node list
+echo "Generating inventories..."
 /bin/bash ./create_inventories.sh
 
 ### Configure Controllers and Nodes with Ansible ###
+echo "Configuring the cluster with Ansible. This may take a few minutes..."
 # Configure common items on all guests; baseline software, nfs, firewall, etc
 ansible-playbook -i inventory ./playbooks/common.yaml
 # Configure controller
@@ -29,8 +65,15 @@ ansible-playbook -i inventory ./playbooks/node.yaml
 ansible-playbook -i inventory ./playbooks/slurm_init.yaml
 
 ### Run some tests to confim ###
+# Get the control node IP address from hosts file
+HPC_CTRL_1_IP=$(grep hpc-control-1 hosts | awk '{print $1}')
+# Is the tests file executable?
+if [ ! -x ./tests.sh ]; then
+    echo "Making tests script executable."
+    chmod +x ./tests.sh
+fi
 # Run a test
-printf "%s\n" "If you see a time and date stamp here, Congratulations! The cluster worked! Feel free to add you own tests."
-NUM_NODES=$(wc -l < compute_nodes)
-sshpass -p vagrant ssh -t -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null vagrant@hpc-control-1 "srun --nodes=$NUM_NODES date"; exit
-# More tests under here
+echo "Running test jobs..."
+/bin/bash ./tests.sh
+# Done
+echo "HPC cluster setup is complete."
---
+++ tests.sh
@@ -0,0 +1,23 @@
+# !/bin/bash
+
+### Gather facts about the cluster ###
+# Get the control node IP address from hosts file
+HPC_CTRL_1_IP=$(grep hpc-control-1 hosts | awk '{print $1}')
+NUM_NODES=$(wc -l < compute_nodes)
+
+# Is this file executable?
+if [ ! -x ./tests.sh ]; then
+    echo "Making tests script executable."
+    chmod +x ./tests.sh
+fi
+
+### Tests to confirm the HPC cluster is working ###
+### Add tests below ###
+printf "%s\n" "If you see a time and date stamp here, Congratulations! The cluster worked! Feel free to add you own tests."
+# Get the time and date from the control node and all compute nodes
+printf "%s\n" "Getting time and date from all nodes in the cluster:"
+sshpass -p vagrant ssh -t -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null vagrant@$HPC_CTRL_1_IP "srun --nodes=$NUM_NODES date"
+
+# Get the hostname from all compute nodes
+printf "%s\n" "Getting hostnames from all nodes in the cluster:"
+sshpass -p vagrant ssh -t -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null vagrant@$HPC_CTRL_1_IP "srun --nodes=$NUM_NODES hostname"
